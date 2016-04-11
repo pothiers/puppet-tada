cfaec01fbfc13cab7e126d3c6152c7c79ea8a17d
Steve Pothier <pothier@noao.edu>
2016-04-11 15:18:23 -0700

    foo

--- manifests/service.pp
+++ manifests/service.pp
@@ -3,7 +3,7 @@
 # https://docs.puppetlabs.com/guides/module_guides/bgtm.html
 
 class tada::service  (
-  $printer  = 'astro',
+  $printer  = 'astro',   
   $cache    = '/var/tada/cache',
   ) {  
   service { 'dqd':
@@ -11,7 +11,7 @@ class tada::service  (
     subscribe => [File ['/etc/tada/dqd.conf',
                         '/etc/init.d/dqd',
                         '/etc/tada/hiera.yaml',
-                        '/etc/tada/tada.conf'
+                        '/etc/tada/tada.conf',
                         ],
                   Class['redis'],
                   Python::Requirements[ '/etc/tada/requirements.txt'],
