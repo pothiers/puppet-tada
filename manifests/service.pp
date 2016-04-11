d471593f787c503e806fabd9e3735401ef898f63
Steve Pothier <pothier@noao.edu>
2016-04-11 14:56:35 -0700

    foo

--- manifests/service.pp
+++ manifests/service.pp
@@ -11,7 +11,7 @@ class tada::service  (
     subscribe => [File ['/etc/tada/dqd.conf',
                         '/etc/init.d/dqd',
                         '/etc/tada/hiera.yaml',
-                        '/etc/tada/tada.conf',
+                        '/etc/tada/tada.conf'
                         ],
                   Class['redis'],
                   Python::Requirements[ '/etc/tada/requirements.txt'],
@@ -25,7 +25,7 @@ class tada::service  (
   service { 'watchpushd':
     ensure    => 'running',
     subscribe => [File ['/etc/tada/watchpushd.conf',
-                        '/etc/init.d/watchpushd'
+                        '/etc/init.d/watchpushd',
                         ],
                   Python::Requirements[ '/etc/tada/requirements.txt'],
                   Package['dataq', 'tada']
