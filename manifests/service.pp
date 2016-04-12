91906a4c213e930f0b21a61bad19a8f06528ab1e
Steve Pothier <pothier@noao.edu>
2016-04-12 10:02:22 -0700

    foo

--- manifests/service.pp
+++ manifests/service.pp
@@ -3,15 +3,15 @@
 # https://docs.puppetlabs.com/guides/module_guides/bgtm.html
 
 class tada::service  (
-  $printer  = 'astro',   
-  $cache    = '/var/tada/cache',
+  $printer  = 'astro',             
+  $cache    = '/var/tada/cache',  
   ) {  
   service { 'dqd':
     ensure   => 'running',
     subscribe => [File ['/etc/tada/dqd.conf',
                         '/etc/init.d/dqd',
                         '/etc/tada/hiera.yaml',
-                        '/etc/tada/tada.conf',
+                        '/etc/tada/tada.conf'
                         ],
                   Class['redis'],
                   Python::Requirements[ '/etc/tada/requirements.txt'],
@@ -25,7 +25,7 @@ class tada::service  (
   service { 'watchpushd':
     ensure    => 'running',
     subscribe => [File ['/etc/tada/watchpushd.conf',
-                        '/etc/init.d/watchpushd',
+                        '/etc/init.d/watchpushd'
                         ],
                   Python::Requirements[ '/etc/tada/requirements.txt'],
                   Package['dataq', 'tada']
