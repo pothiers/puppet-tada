99ad051f07bec54430f7fb66374d43a3b4f79387
Steve Pothier <pothier@noao.edu>
2016-06-15 09:15:53 -0700

    snap

--- files/audit-schema.sql
+++ files/audit-schema.sql
@@ -1,11 +1,14 @@
 CREATE TABLE audit(
-	telescope not null,
+	md5sum,         -- of FITS file
+	obsday date,	-- observation day
+	telescope not null, 
 	instrument not null,
-	srcpath not null,
-	recorded,
-	submitted,
-	success,
-	archerr,
-	archfile,
+	srcpath not null,	-- full path of FITS file
+	recorded timestamp,	-- when srcpath was recorded by dome
+	submitted timestamp,	-- when submitted to archive 
+	success,		-- NULL until ingest attempted. Then True iff
+				-- archive reported success on ingest
+	archerr,	-- Error message (if applicable) of Archive Ingest
+	archfile,	-- basename of FITS file in Archive
 	PRIMARY KEY (telescope, instrument, srcpath)
 	);
